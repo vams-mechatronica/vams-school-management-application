03937352a621c5bc30d53c92b5bb082ee62c6c69
vandanapra <112847023+prakashvand@users.noreply.github.com>
2024-08-23 00:03:53 +0530

    FIX url

--- apps/students/urls.py
+++ apps/students/urls.py
@@ -14,7 +14,7 @@ from .views import (
 urlpatterns = [
     path("list", StudentListView.as_view(), name="student-list"),
     path("<int:pk>/", StudentDetailView.as_view(), name="student-detail"),
-    path("<int:pk>/", StudentDashboardView.as_view(), name="student-dashboard"),
+    path("dashboard/<int:pk>/", StudentDashboardView.as_view(), name="student-dashboard"),
     path("create/", StudentCreateView.as_view(), name="student-create"),
     path("<int:pk>/update/", StudentUpdateView.as_view(), name="student-update"),
     path("delete/<int:pk>/", StudentDeleteView.as_view(), name="student-delete"),
