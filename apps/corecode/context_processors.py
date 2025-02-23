393472e407a18c45a31d3e46577669bcf3e264a3
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-02-24 00:40:54 +0530

    FIX letterhead

--- apps/corecode/context_processors.py
+++ apps/corecode/context_processors.py
@@ -5,6 +5,7 @@ def site_defaults(request):
     current_session = AcademicSession.objects.get(current=True)
     current_term = AcademicTerm.objects.get(current=True)
     site_config = SchoolDetail.objects.first()
+    
     contexts = {
         "current_session": current_session.name,
         "current_term": current_term.name,
@@ -12,6 +13,6 @@ def site_defaults(request):
         "school_short_name":site_config.short_name if site_config else "My School",
         "slogan":site_config.slogan if site_config else "My School",
         "address":site_config.address if site_config else "My School",
-        "letterheader_image":site_config.letterhead.url if site_config else "#",
+        "letterheader_image":site_config.letterhead.url if site_config and site_config.letterhead else "#",
     }
     return contexts
