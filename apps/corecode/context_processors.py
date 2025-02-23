03266696237ac98ca8a7f02628d02302c81926da
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-02-23 23:36:58 +0530

    ADD site-config contextprocessor

--- apps/corecode/context_processors.py
+++ apps/corecode/context_processors.py
@@ -9,6 +9,7 @@ def site_defaults(request):
         "current_session": current_session.name,
         "current_term": current_term.name,
         "school_name":site_config.name,
+        "school_short_name":site_config.short_name,
         "slogan":site_config.slogan,
         "address":site_config.address,
     }
