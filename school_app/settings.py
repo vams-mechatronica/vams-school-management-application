8b5b3e85a3d100e95d606ae100a8c7f0d1e3d609
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-02-25 08:24:24 +0530

    ADD contrib sites

--- school_app/settings.py
+++ school_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "django.contrib.sites",
     "django.contrib.auth",
     "django.contrib.admin",
     "django.contrib.contenttypes",
