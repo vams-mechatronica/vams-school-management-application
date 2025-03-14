4401d671e8afd32894c4337398ea09c0ec251c96
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-03-14 15:11:03 +0530

    UPDATE Middle ware

--- apps/corecode/middleware.py
+++ apps/corecode/middleware.py
@@ -6,8 +6,8 @@ class SiteWideConfigs:
         self.get_response = get_response
 
     def __call__(self, request):
-        current_session = AcademicSession.objects.get(current=True)
-        current_term = AcademicTerm.objects.get(current=True)
+        current_session,_ = AcademicSession.objects.get_or_create(current=True)
+        current_term,_ = AcademicTerm.objects.get_or_create(current=True)
 
         request.current_session = current_session
         request.current_term = current_term
