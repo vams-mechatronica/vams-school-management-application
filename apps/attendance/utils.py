393e4c23f93ac2602777114819986e6d04781fe8
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-05-22 16:33:51 +0530

    FIX email sent logic

--- apps/attendance/utils.py
+++ apps/attendance/utils.py
@@ -4,24 +4,28 @@ from django.utils import timezone
 from apps.email_module.modules import send_html_email_async
 from django.conf import settings
 from .models import StaffAttendance
+import logging
+logger = logging.getLogger(__name__)
 
 def send_signout_reminder():
     now = timezone.now()
     today = now.date()
+    
 
     # You may filter by time to send it only after a certain time of day
-    if now.hour < 13:  # Only run this after 1 PM
+    if now.hour > 13:  # Only run this after 1 PM
         return
 
     # Get attendances with time_in but no time_out
-    missing_signouts = StaffAttendance.objects.filter(date=today, time_in__isnull=False, time_out__isnull=True)
+    missing_signouts = StaffAttendance.objects.filter(date=today, status=1, time_out__isnull=True)
     sitename = getattr(settings, 'SITE_NAME', 'School Portal')
 
     for record in missing_signouts:
-        user = record.staff_user.user  # Assuming StaffAttendance has staff_user FK → Staff → user
+        user = record.staff.user  # Assuming StaffAttendance has staff_user FK → Staff → user
         email = user.email
         if email:
-            send_html_email_async(to_emails=[email],template_name="signout_remider",content_context={
+            logger.info("sending email to: {}".format(email))
+            send_html_email_async(to_emails=[email],template_name="signout_reminder",content_context={
                 "user_name": user.get_full_name() or user.username,
                 "date": timezone.now().date().strftime('%B %d, %Y'),
                 "year": timezone.now().year,
--- apps/email_module/admin.py
+++ apps/email_module/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import EmailTemplate
+from .models import EmailTemplate, EmailSentLogs
 
 # Register your models here.
 @admin.register(EmailTemplate)
 class EmailTemplateAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(EmailSentLogs)
+class EmailSentLogsAdmin(admin.ModelAdmin):
+    list_display = ('template_name','to_emails','sent_at')
+    
+
     
